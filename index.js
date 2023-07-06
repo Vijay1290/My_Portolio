9515f41618bb0ecfeeb3eb71c77f6a8de0704a65
VijaySabrole <100029922+Vijay1290@users.noreply.github.com>
2023-07-07 00:41:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var i = 0;
-var txt = "I'm Vijay Sabrole"; 
-var speed = 400; 
+var txt = "I'm Vijay Sabrole";
+var speed = 400;
+
 function typeWriter() {
     var type = document.getElementById("type");
     var cursor = document.getElementById("cursor");
@@ -8,10 +9,24 @@ function typeWriter() {
         type.innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
-    }
-
-    else {
+    } else {
         cursor.style.display = "none";
     }
 }
 window.addEventListener('DOMContentLoaded', typeWriter);
+
+
+function toggleView() {
+    var section = document.getElementById("view_more");
+    var button = document.querySelector(".view_button");
+
+    if (section.style.display === "none") {
+        section.style.display = "flex";
+        button.textContent = "View Less";
+        AOS.init();
+
+    } else {
+        section.style.display = "none";
+        button.textContent = "View More";
+    }
+}
